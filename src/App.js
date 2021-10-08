62c38d5eafd1ec0cb8ea94a7ae244d803d878060
rohitkumar0427 <61402931+rohitkumar0427@users.noreply.github.com>
2021-10-08 19:55:15 +0530

    Working Router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { VocabList } from "./VocabList";
+import { Switch, Route, NavLink } from "react-router-dom";
+import Grid from "@mui/material/Grid";
 
 function App() {
-  const randomNum = Math.floor(Math.random() * Math.floor(VocabList.length));
-
   return (
     <div className="App">
+      <Grid container>
+        <Grid item>
+          <NavLink to="/NotesApp">NotesApp</NavLink>
+        </Grid>
+        <Grid item>
+          <NavLink to="/WebsiteBlocker">WebsiteBlocker</NavLink>
+        </Grid>
+        <Grid item>
+          <NavLink to="/Bookmarks">Bookmarks</NavLink>
+        </Grid>
+        <Grid item>
+          <NavLink to="/Remainder">Remainder</NavLink>
+        </Grid>
+      </Grid>
       <Switch>
-        <Route path="/NotesApp">
-        
-        </Route>
-        <Route path="/Bookmarks">
-
-        </Route>
-        <Route path="/Remainder">
-
-        </Route>
-        <Route path="/WebsiteBlocker">
-
-        </Route>
+        <Route path="/NotesApp">HI</Route>
+        <Route path="/Bookmarks">Bro</Route>
+        <Route path="/Remainder">How</Route>
+        <Route path="/WebsiteBlocker">Are</Route>
       </Switch>
     </div>
   );
