3db708b92501491e8cd3c999f4e75b3a9cecca3f
rohitkumar0427 <61402931+rohitkumar0427@users.noreply.github.com>
2021-10-08 19:34:45 +0530

    Initial Push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { VocabList } from "./VocabList";
 
 function App() {
+  const randomNum = Math.floor(Math.random() * Math.floor(VocabList.length));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route path="/NotesApp">
+        
+        </Route>
+        <Route path="/Bookmarks">
+
+        </Route>
+        <Route path="/Remainder">
+
+        </Route>
+        <Route path="/WebsiteBlocker">
+
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/VocabList.js
@@ -0,0 +1,42 @@
+export const VocabList = [
+  {
+    FIELD1: "abacus",
+    FIELD2: "frame with balls for calculating       ",
+  },
+  {
+    FIELD1: "abate",
+    FIELD2: "to lessen to subside        ",
+  },
+  {
+    FIELD1: "abdication",
+    FIELD2: "giving up control authority        ",
+  },
+  {
+    FIELD1: "aberration",
+    FIELD2: "straying away from what is normal      ",
+  },
+  {
+    FIELD1: "abet",
+    FIELD2: "help/encourage smb (in doing wrong)     ",
+  },
+  {
+    FIELD1: "abeyance",
+    FIELD2: "suspended action          ",
+  },
+  {
+    FIELD1: "abhor",
+    FIELD2: "to hate to detest        ",
+  },
+  {
+    FIELD1: "abide",
+    FIELD2: "be faithful to endure        ",
+  },
+  {
+    FIELD1: "abjure",
+    FIELD2: "promise or swear to give up      ",
+  },
+  {
+    FIELD1: "abraded",
+    FIELD2: "rubbed off worn away by friction      ",
+  },
+];
